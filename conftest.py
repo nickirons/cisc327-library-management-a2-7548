a58c00fe3200ca6b5da2633cac5a312954ec8ebf
Nicholas Irons <nicholasirons@Nicks-Macbook-Pro.local>
2025-12-08 12:06:14 -0500

    add github

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ import database
 @pytest.fixture(autouse=True)
 def temp_db(tmp_path, monkeypatch):
     """
-    Use a fresh temporary SQLite database for each test to keep tests isolated.
+    Use a fresh temporary sql database for each test so they stay keep tests isolated.
     """
     db_path = tmp_path / "test.db"
     monkeypatch.setattr(database, "DATABASE", str(db_path))
